5c4e77ad7228a0fb27e6021f26d8ff3fa49e7b60
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-03-31 10:23:46 +0300

    Add comment block for Task-5 description
    Added a comment block to describe the task for counting positive and negative numbers.

---
+++ Lab_3/Task_5.cpp
@@ -0,0 +1,10 @@
+/*
+Task-5: Count Positive & Negative Numbers
+
+int pos=0, neg=0;
+for(int i=0;i<n;i++)
+    for(int j=0;j<m;j++){
+        if(A[i][j] > 0) pos++;
+        else if(A[i][j] < 0) neg++;
+    }
+*/
